be173efdcbc7cfb2a80f33abc4d216bd327918be
travail <sawa.tomoyuki@gmail.com>
2013-03-18 12:37:05 +0900

    To use ruby-mode when edit Gemfile

--- .emacs.d/conf/init-ruby.el
+++ .emacs.d/conf/init-ruby.el
@@ -1,7 +1,8 @@
 ;; ruby-mode
 (autoload 'ruby-mode "ruby-mode")
 
-(setq auto-mode-alist (append '(("Rakefile$" . cperl-mode)) auto-mode-alist))
+(setq auto-mode-alist (append '(("Rakefile$" . ruby-mode)) auto-mode-alist))
+(setq auto-mode-alist (append '(("Gemfile$" . ruby-mode)) auto-mode-alist))
 
 ;; ruby-electric.el
 (require 'ruby-electric)
