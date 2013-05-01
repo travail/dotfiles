9c4485431d32cc34313a5d9e4e7e38aac4e4c76b
travail <sawa.tomoyuki@gmail.com>
2013-05-02 02:57:34 +0900

    Modify .emacs to treat .json files as javascript

--- .emacs.d/conf/init-javascript.el
+++ .emacs.d/conf/init-javascript.el
@@ -1,3 +1,4 @@
 ;; javascript-mode
 (autoload 'javascript-mode "javascript.el" "JavaScript mode" t)
 (add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
