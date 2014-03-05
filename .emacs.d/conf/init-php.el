7db4f5a04ae6a01ae2c78d95e5e2330bd5d8ca0d
travail <sawa.tomoyuki@gmail.com>
2014-03-05 12:41:43 +0900

    Add flymake for php

--- .emacs.d/conf/init-php.el
+++ .emacs.d/conf/init-php.el
@@ -19,3 +19,33 @@
               (add-to-list 'ac-sources 'ac-source-php-completion)
               (auto-complete-mode t)
               )))
+
+;; flymake
+(require 'flymake-php)
+(eval-after-load 'flymake
+  '(progn
+     (set-face-background 'flymake-errline "red")
+     (set-face-background 'flymake-warnline "blue")
+     ))
+(add-hook 'php-mode-hook 'flymake-php-load)
+
+;; collecting all functions for compaletion
+(defun my-fetch-php-completions ()
+  (if (and (boundp 'my-php-symbol-list)
+           my-php-symbol-list)
+      my-php-symbol-list
+
+    (message "Fetching completion list...")
+
+    (with-current-buffer
+        (url-retrieve-synchronously "http://www.php.net/manual/en/indexes.functions.php")
+
+      (goto-char (point-min))
+
+      (message "Collecting function names...")
+
+      (setq my-php-symbol-list nil)
+      (while (re-search-forward "<a[^>]*class=\"index\"[^>]*>\\([^<]+\\)</a>" nil t)
+        (push (match-string-no-properties 1) my-php-symbol-list))
+
+      my-php-symbol-list)))
