fa278d22af09843076d2de62a7559a86fcebd499
travail <sawa.tomoyuki@gmail.com>
2015-01-09 22:45:10 +0900

    Good bye ruby-electric

---
+++ .emacs.d/elpa/ruby-end-0.3.1/ruby-end-autoloads.el
@@ -0,0 +1,29 @@
+;;; ruby-end-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (ruby-end-mode) "ruby-end" "ruby-end.el" (21676
+;;;;;;  40219 409857 315000))
+;;; Generated autoloads from ruby-end.el
+
+(autoload 'ruby-end-mode "ruby-end" "\
+Automatic insertion of end blocks for Ruby.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ruby-end-pkg.el") (21676 40219 477201
+;;;;;;  988000))
+
+;;;***
+
+(provide 'ruby-end-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ruby-end-autoloads.el ends here
