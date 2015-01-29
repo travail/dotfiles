6d16c77923ed800e3aa02c017fcf65cd1f05f615
travail <sawa.tomoyuki@gmail.com>
2015-01-30 00:49:02 +0900

    Fix php-mode

--- .emacs.d/conf/init-php.el
+++ .emacs.d/conf/init-php.el
@@ -11,6 +11,18 @@
             (c-set-offset 'arglist-intro' 4)
             (c-set-offset 'arglist-cont-nonempty' 4)
             (c-set-offset 'arglist-close' 0)
+
+            (defun ywb-php-lineup-arglist-intro (langelem)
+              (save-excursion
+                (goto-char (cdr langelem))
+                (vector (+ (current-column) c-basic-offset))))
+            (defun ywb-php-lineup-arglist-close (langelem)
+              (save-excursion
+                (goto-char (cdr langelem))
+                (vector (current-column))))
+            (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+            (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)
+
             (require 'php-completion)
             (php-completion-mode t)
             (define-key php-mode-map (kbd "C-c cmp") 'phpcmp-complete)
