2436681843f5efdd309515dcc4cd7a6f8c504ac3
travail <sawa.tomoyuki@gmail.com>
2015-10-01 00:01:16 +0900

    Introduce helm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,25 +2,46 @@
 ;; (setq inhibit-default-init t)
 
 ;;
-(add-to-list 'load-path "~/.emacs.d/lisp")
-(add-to-list 'load-path "~/.emacs.d/lisp/auto-install")
-
-;;
 (when (< emacs-major-version 23)
   (defvar user-emacs-directory "~/.emacs.d/"))
 
 ;;
 (defun add-to-load-path (&rest paths)
   (let (path)
-	(dolist (path paths paths)
-	  (let ((default-directory
-			  (expand-file-name (concat user-emacs-directory path))))
-		(add-to-list 'load-path default-directory)
-		(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-			(normal-top-level-add-subdirs-to-load-path))))))
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
 
-;; load configuration files
-(add-to-load-path "lisp/conf" "lisp/elpa")
+;; configuration directories
+(add-to-load-path "lisp/conf" "lisp/el-get" "lisp/github")
+
+;; el-get
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+(add-to-list 'load-path (locate-user-emacs-file "lisp/el-get"))
+(setq el-get-dir (locate-user-emacs-file "lisp/el-get"))
+(require 'el-get)
+(el-get-bundle auto-complete)
+(el-get-bundle migemo)
+(el-get-bundle helm)
+(el-get-bundle helm-swoop)
+(el-get-bundle smartparens)
+(el-get-bundle flymake)
+(el-get-bundle flymake-php)
+(el-get-bundle flymake-easy)
+(el-get-bundle cperl-mode)
+(el-get-bundle perl-completion)
+(el-get-bundle ruby-mode)
+(el-get-bundle ruby-end)
+(el-get-bundle php-mode)
+(el-get-bundle css-mode)
+(el-get-bundle javascript)
+(el-get-bundle php-completion)
+(el-get-bundle yaml-mode)
+
 (load "init-c")
 (load "init-html")
 (load "init-javascript")
@@ -29,14 +50,7 @@
 (load "init-ruby")
 (load "init-yaml")
 (load "init-japanese")
-;; (load "flymake")
-
-;; auto-install
-(require 'auto-install)
-(add-to-list 'load-path auto-install-directory)
-;;(auto-install-update-emacswiki-package-name t)
-(auto-install-compatibility-setup)
-(setq ediff-window-setup-function 'ediff-setup-window-plain)
+(load "flymake")
 
 ;; auto-complete
 (require 'auto-complete-config)
@@ -46,30 +60,27 @@
 (setq ac-delay 0.05)
 (setq ac-auto-show-menu 0.05)
 
-;; anything
-(require 'anything-startup)
+;; migemo
+;; (require 'migemo)
+;; (setq migemo-command "cmigemo")
+;; (setq migemo-options '("-q" "--emacs"))
+;; (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+;; (setq migemo-user-dictionary nil)
+;; (setq migemo-regex-dictionary nil)
+;; (setq migemo-coding-system 'utf-8-unix)
+;; (load-library "migemo")
+;; (migemo-init)
 
-;; anything-exuberant-ctags
-(when (require 'anything nil t)
-  (require 'anything-exuberant-ctags)
-  )
-(define-key global-map (kbd "C-c j")  'anything-exuberant-ctags-select-from-here)
-
-;; yasnippet
-;; (require 'yasnippet)
-;; (yas-global-mode 1)
-
-;; ;; import PATH from shell
-;; (defun set-exec-path-from-shell-PATH ()
-;;   "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
-
-;; This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
-;;   (interactive)
-;;   (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-;;     (setenv "PATH" path-from-shell)
-;;     (setq exec-path (split-string path-from-shell path-separator))))
-
-;; (set-exec-path-from-shell-PATH)
+;; helm
+(require 'helm-config)
+(helm-mode +1)
+(define-key global-map (kbd "C-s") 'helm-swoop)
+(define-key global-map (kbd "C-c C-h") 'execute-extended-command)
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
 ;; turn on font-lock mode
 (global-font-lock-mode t)
@@ -125,12 +136,12 @@
 ;;; interfacing with ELPA, the package archive.
 ;;; Move this code earlier if you want to reference
 ;;; packages in your .emacs.
-;;(when
-;;	(load
-;;	 (expand-file-name "~/.emacs.d/elpa/package.el"))
+;; (when
+;; 	(load
+;; 	 (expand-file-name "~/.emacs.d/elpa/package.el"))
 ;;  (package-initialize))
-(when (require 'package nil t)
-  (add-to-list 'package-archives
-			   '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
-  (package-initialize))
+;; (when (require 'package nil t)
+;;   (add-to-list 'package-archives
+;; 			   '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;   (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+;;   (package-initialize))
