b9d6fca184c19d03bfdba2d78bbf658f89e89b3c
travail <sawa.tomoyuki@gmail.com>
2015-10-02 13:13:18 +0900

    Push out configuration in init.el to lisp/conf

---
+++ .emacs.d/lisp/conf/20_auto-complete.el
@@ -0,0 +1,14 @@
+;;; package --- 20_auto-complete.el
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'auto-complete-config)
+(global-auto-complete-mode 1)
+(setq ac-ignore-case t)
+(setq ac-auto-start 2)
+(setq ac-delay 0.05)
+(setq ac-auto-show-menu 0.05)
+
+;;; 20_auto-complete.el ends here
