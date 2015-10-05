e045cd113e2ec853deca3981b195a26ec3a3e997
travail <sawa.tomoyuki@gmail.com>
2015-10-05 18:51:29 +0900

    Make flycheck work with Perl

--- emacs.d/lisp/conf/20_flycheck.el
+++ emacs.d/lisp/conf/20_flycheck.el
@@ -1,15 +1,15 @@
-;;; package --- :
+;;; package --- : 20_flycheck.el
+
 ;;; Commentary:
+
 ;;; Code:
 
+(require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (eval-after-load 'flycheck
   '(custom-set-variables
     '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
-
-;; Perl
-(add-hook 'perl-mode-hook 'flycheck-mode)
 
 ;; Ruby
 (add-hook 'ruby-mode-hook 'flycheck-mode)
