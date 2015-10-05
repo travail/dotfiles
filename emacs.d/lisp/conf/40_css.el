6c27dfc988024b7bee16ed0fcaee76d3206c41aa
travail <sawa.tomoyuki@gmail.com>
2015-10-05 13:35:37 +0900

    Introduce css-mode and scss-mode

--- emacs.d/lisp/conf/40_css.el
+++ emacs.d/lisp/conf/40_css.el
@@ -1,3 +1,25 @@
-;; css-mode
-(autoload 'javascript-mode "css-mode.el" "CSS mode" t)
-(add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
+;;; package --- 40_css.el
+
+;;; Commentary:
+
+;;; Code:
+
+(autoload 'css-mode "css-mode")
+(setq auto-mode-alist
+      (cons '("\\.css\\'" . css-mode) auto-mode-alist))
+
+(require 'scss-mode)
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.css$" . scss-mode))
+
+(defun scss-custom ()
+  "scss-mode-hook"
+  (and
+   (set (make-local-variable 'css-indent-offset) 2)
+   (set (make-local-variable 'scss-compile-at-save) nil)
+   )
+  )
+(add-hook 'scss-mode-hook
+  '(lambda() (scss-custom)))
+
+;;; 40_css.el ends here
