c4dbda44dba744739275e5e152cdc4c00d7c5d84
travail <sawa.tomoyuki@gmail.com>
2015-10-06 17:15:44 +0900

    Add configuration about phpcs and phpmd

--- emacs.d/lisp/conf/40_php.el
+++ emacs.d/lisp/conf/40_php.el
@@ -1,4 +1,9 @@
-;; php-mode
+;;; package --- 40_php.el
+
+;;; Commentary:
+
+;;; Code:
+
 (autoload 'php-mode "php-mode")
 (setq auto-mode-alist
       (cons '("\\.php\\'" . php-mode) auto-mode-alist))
@@ -25,30 +30,18 @@
 
             (require 'php-completion)
             (php-completion-mode t)
-            (define-key php-mode-map (kbd "C-c cmp") 'phpcmp-complete)
             (when (require 'auto-complete nil t)
               (make-variable-buffer-local 'ac-sources)
               (add-to-list 'ac-sources 'ac-source-php-completion)
               (auto-complete-mode t)
               )))
 
-;; collecting all functions for compaletion
-(defun my-fetch-php-completions ()
-  (if (and (boundp 'my-php-symbol-list)
-           my-php-symbol-list)
-      my-php-symbol-list
+(add-hook 'php-mode-hook
+          (lambda ()
+            (setq flycheck-phpcs-standard "PSR2")
+            (setq flycheck-phpmd-rulesets '("unusedcode"))
+            (setq flycheck-php-phpmd-executable "~/bin/phpmd")
+            (setq flycheck-php-phpcs-executable "~/bin/phpcs")
+            ))
 
-    (message "Fetching completion list...")
-
-    (with-current-buffer
-        (url-retrieve-synchronously "http://www.php.net/manual/en/indexes.functions.php")
-
-      (goto-char (point-min))
-
-      (message "Collecting function names...")
-
-      (setq my-php-symbol-list nil)
-      (while (re-search-forward "<a[^>]*class=\"index\"[^>]*>\\([^<]+\\)</a>" nil t)
-        (push (match-string-no-properties 1) my-php-symbol-list))
-
-      my-php-symbol-list)))
+;;; 40_php.el
