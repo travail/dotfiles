c465d9910bb53eb7af171d7e6b2592e01883802b
travail <sawa.tomoyuki@gmail.com>
2015-10-14 17:05:08 +0900

    Add theme solarized

--- emacs.d/lisp/conf/20_theme.el
+++ emacs.d/lisp/conf/20_theme.el
@@ -1,10 +1,23 @@
-(load-theme 'misterioso)
-(custom-theme-set-faces
- 'misterioso
- ;; background
- '(cursor ((t (:foreground "#F8F8F0"))))
- '(default ((t (:background "#1B1D1E" :foreground "#F8F8F2"))))
+;;; package --- 20_theme.el
 
- ;; current line
- '(hl-line ((t (:background "color-238"))))
- )
+;;; Commentary:
+
+;;; Code:
+
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "/lisp/theme/solarized"))
+(load-theme 'solarized t)
+(set-frame-parameter nil 'background-mode 'dark)
+(set-terminal-parameter nil 'background-mode 'dark)
+
+;; (load-theme 'misterioso)
+;; (custom-theme-set-faces
+;;  'misterioso
+;;  ;; background
+;;  '(cursor ((t (:foreground "#F8F8F0"))))
+;;  '(default ((t (:background "#1B1D1E" :foreground "#F8F8F2"))))
+
+;;  ;; current line
+;;  '(hl-line ((t (:background "color-238"))))
+;;  )
+
+;;; 20_theme.el ends here
