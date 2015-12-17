32a2f652713b2ec2ddaf306f178e68316ed5e83a
travail <sawa.tomoyuki@gmail.com>
2015-12-17 16:52:13 +0900

    Remove peco

--- zsh/peco/select-history.sh
+++
@@ -1,7 +0,0 @@
-funciton peco-select-history() {
-    BUFFER=$(history -r -n 1 | peco --query "$LBUFFER")
-    CURSOR=$#BUFFER
-    zle clear-screen
-}
-zle -N peco-select-history
-bindkey '^r' peco-select-history
