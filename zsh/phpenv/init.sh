0fab82b337d12c4f5333ae01c4e0fe3d13d205b5
travail <sawa.tomoyuki@gmail.com>
2017-10-11 14:02:52 +0000

    Remove phpenv

--- zsh/phpenv/init.sh
+++
@@ -1,8 +0,0 @@
-export PHPENV_ROOT=/usr/local/phpenv
-export PATH="$PHPENV_ROOT/bin:$PHPENV_ROOT/shims:$PATH"
-EXECUTABLE_PATH="$PHPENV_ROOT/bin/phpenv"
-if [ -x "$EXECUTABLE_PATH" ]; then
-    eval "$(phpenv init -)"
-else
-    echo "Executable $EXECUTABLE_PATH not found"
-fi
