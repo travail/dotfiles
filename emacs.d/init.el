b319fd9d17ba11812d242dbbca583460a4ba84c1
travail <sawa.tomoyuki@gmail.com>
2019-03-29 02:30:38 +0000

    Remove company-go

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -82,13 +82,6 @@
 (exec-path-from-shell-initialize)
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
- '(package-selected-packages (quote (company-go))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
