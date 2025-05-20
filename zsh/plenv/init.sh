5d7f764d1b2dcec7f3fea6f9ab307d17a7a486d1
travail <sawa.tomoyuki@gmail.com>
2025-05-20 22:10:02 +0900

    Disable plenv and rbenv

--- zsh/plenv/init.sh
+++ zsh/plenv/init.sh
@@ -1,8 +1,8 @@
-COMMAND=plenv
-export PLENV_ROOT=/usr/local/plenv
-export PATH="$PLENV_ROOT/bin:$PLENV_ROOT/shims:$PATH"
-if type $COMMAND > /dev/null 2>&1; then
-    eval "$($COMMAND init -)"
-else
-    echo "$COMMAND not found"
-fi
+# COMMAND=plenv
+# export PLENV_ROOT=/usr/local/plenv
+# export PATH="$PLENV_ROOT/bin:$PLENV_ROOT/shims:$PATH"
+# if type $COMMAND > /dev/null 2>&1; then
+#     eval "$($COMMAND init -)"
+# else
+#     echo "$COMMAND not found"
+# fi
--- zsh/rbenv/init.sh
+++ zsh/rbenv/init.sh
@@ -1,8 +1,8 @@
-COMMAND=rbenv
-export RBENV_ROOT=/usr/local/rbenv
-export PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"
-if type $COMMAND > /dev/null 2>&1; then
-    eval "$($COMMAND init -)"
- else
-    echo "$COMMAND not found"
-fi
+# COMMAND=rbenv
+# export RBENV_ROOT=/usr/local/rbenv
+# export PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"
+# if type $COMMAND > /dev/null 2>&1; then
+#     eval "$($COMMAND init -)"
+#  else
+#     echo "$COMMAND not found"
+# fi
