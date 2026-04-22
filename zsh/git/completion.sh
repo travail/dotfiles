1d3f2fb1993c69e8c5d4961d2fa9f0b3ed4d5642
travail <sawa.tomoyuki@gmail.com>
2026-04-22 14:00:35 +0900

    Rename git-lc/rc to git-checkout-local/remote with symlinks and completion
    - Rename git-lc to git-checkout-local, keep git-cl as symlink
    - Rename git-rc to git-checkout-remote, keep git-cr as symlink
    - Register checkout-local and checkout-remote in zsh tab completion

--- zsh/git/completion.sh
+++ zsh/git/completion.sh
@@ -1,4 +1,6 @@
 # Add descriptions for custom git aliases in tab completion
 zstyle ':completion:*:*:git:*' user-commands \
-  'lc:checkout local branch with fzf' \
-  'rc:checkout remote branch with fzf'
+  'cl:checkout local branch with fzf' \
+  'checkout-local:checkout local branch with fzf' \
+  'cr:checkout remote branch with fzf' \
+  'checkout-remote:checkout remote branch with fzf'
