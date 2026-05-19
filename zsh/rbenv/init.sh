87745d938f61341c8809f05ea942010bb7887425
travail <sawa.tomoyuki@gmail.com>
2026-05-19 22:14:31 +0900

    Remove plenv and rbenv (unused)

--- zsh/rbenv/init.sh
+++
@@ -1,8 +0,0 @@
-# COMMAND=rbenv
-# export RBENV_ROOT=/usr/local/rbenv
-# export PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"
-# if type $COMMAND > /dev/null 2>&1; then
-#     eval "$($COMMAND init -)"
-#  else
-#     echo "$COMMAND not found"
-# fi
