2c9cf34d24fb944194fe5238eabfbf5b3e6ffe4c
travail <sawa.tomoyuki@gmail.com>
2026-05-20 19:09:53 +0900

    Unify fzf key bindings to lowercase (^xf)

--- zsh/fzf/bindkey.sh
+++ zsh/fzf/bindkey.sh
@@ -1,5 +1,5 @@
 # fzf key bindings
-bindkey '^Xf' fzf-file-widget      # ファイル検索（複数選択可）
+bindkey '^xf' fzf-file-widget      # ファイル検索（複数選択可）
 bindkey '^xd' fzf-cdr              # cdr ディレクトリ移動
 bindkey '^xb' fzf-git-checkout     # git ブランチ切り替え
 bindkey '^xl' fzf-git-log          # git log 検索
