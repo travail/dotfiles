ddfc40156ace682fba35f3316f2ebb983ea677e9
travail <sawa.tomoyuki@gmail.com>
2026-06-22 18:52:07 +0900

    op/init.sh: show setup instructions when plugins not initialized

--- zsh/op/init.sh
+++ zsh/op/init.sh
@@ -1,3 +1,7 @@
 if [[ -f ~/.config/op/plugins.sh ]]; then
     source ~/.config/op/plugins.sh
+elif command -v op &>/dev/null; then
+    echo "1Password shell plugins not initialized. Run the following:"
+    echo "  op plugin init aws"
+    echo "  op plugin init gh"
 fi
